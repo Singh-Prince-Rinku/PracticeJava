8f66fe287694911e36815abeabe2aba7fb7a598e
Singh-Prince-Rinku <sahilsingh000578@gmail.com>
2024-03-06 23:12:54 +0530

    update

---
+++ Pac1.java
@@ -0,0 +1,9 @@
+import PackageOne.PkgOne;
+
+public class Pac1 {
+    public static void main(String[] args) {
+        PkgOne p1 = new PkgOne();
+        p1.setData(5,6);
+        p1.display();
+    }
+}
---
+++ PackageOne/PkgOne.java
@@ -0,0 +1,16 @@
+package PackageOne;
+
+public class PkgOne {
+    int x;
+    int y;
+
+    public void setData(int p, int q) {
+        x = p;
+        y = q;
+    }
+
+    public void display() {
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
