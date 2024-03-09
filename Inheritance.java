efe385e179030a288986ce11bb73f26d87ed06f9
Singh-Prince-Rinku <sahilsingh000578@gmail.com>
2024-03-09 11:02:21 +0530

    updated

---
+++ Inheritance.java
@@ -0,0 +1,18 @@
+class Samsung{
+    int Camera;
+    int Screen;
+    int Zoom;
+    void resolution(){
+        System.out.println("Parent Function Executed");
+    }
+}
+class Vivo extends Samsung{
+    void display(){
+        System.out.println("Child Class Executed");
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        
+    }
+}
