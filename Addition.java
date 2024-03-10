5e7d259d3c0bae0ad9272279cb2ae5ac9d52e095
Singh-Prince-Rinku <sahilsingh000578@gmail.com>
2024-03-10 21:26:53 +0530

    updated

---
+++ Addition.java
@@ -0,0 +1,10 @@
+import PackageTwo.PackTwo;
+public class Addition {
+    public static void main(String[] args){
+        PackTwo p2 = new PackTwo();
+        p2.test();
+        p2.display();
+
+        
+    }
+}
---
+++ PackageTwo/PackTwo.java
@@ -0,0 +1,15 @@
+package PackageTwo;
+
+public class PackTwo {
+    int p = 50;
+    int q = 100;
+    int a;
+
+    public void test() {
+        a = p + q;
+    }
+
+    public void display() {
+        System.out.println(p + " " + q + " :" + a);
+    }
+}
