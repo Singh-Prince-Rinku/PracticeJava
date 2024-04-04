71db7f182f623b775afc194d10e6c49b70c5eb17
Singh-Prince-Rinku <sahilsingh000578@gmail.com>
2024-04-04 22:00:13 +0530

    xyz

---
+++ Experiment.java
@@ -0,0 +1,20 @@
+public class Experiment {
+    public static void main(String[] args) {
+        // Declaring and initializing a 3D array
+        int[][][] threeDArray = {
+            { {1, 2}, {3, 4} },
+            { {5, 6}, {7, 8} }
+        };
+
+        // Accessing and printing elements of the 3D array
+        for (int i = 0; i < threeDArray.length; i++) {
+            for (int j = 0; j < threeDArray[i].length; j++) {
+                for (int k = 0; k < threeDArray[i][j].length; k++) {
+                    System.out.print(threeDArray[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
