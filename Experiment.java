00a1e235fe96d2599da188082fd512012fc844b3
Singh-Prince-Rinku <sahilsingh000578@gmail.com>
2024-04-05 19:11:53 +0530

    xyz

--- Experiment.java
+++ Experiment.java
@@ -1,6 +1,5 @@
 public class Experiment {
     public static void main(String[] args) {
-        // Declaring and initializing a 3D array
         int[][][] threeDArray = {
             { {1, 2}, {3, 4} },
             { {5, 6}, {7, 8} }
